6affa4f18dae21dab4ebe95a0ca47102eeec79e1
Tomas Bermudez <bermudeztomas06@gmail.com>
2025-10-23 17:25:07 -0500

    ahora si, corregido el problema de que el email y el username tambien deben ser únicos

--- Proyecto/SAFE/accounts/views.py
+++ Proyecto/SAFE/accounts/views.py
@@ -3,14 +3,14 @@ from django.views.decorators.http import require_POST
 from . import models
 
 def index(request):
-    usuarios = models.AppUser.objects.order_by("id").values("id", "first_name", "email")
+    usuarios = models.AppUser.objects.order_by("id").values("id", "username", "email")
     return render(request, "accounts/index.html", {"usuarios": list(usuarios)})
 
 @require_POST
 def user_add(request):
     username = request.POST.get("username", "").strip()
     email = request.POST.get("email", "").strip()
-    if username:
+    if username and email:
         models.AppUser.objects.create(username=username, email=email)
     return redirect("index")
 
