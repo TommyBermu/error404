ed8f2b01aee91f5fbe3b86baba0df0dc35c55c66
Tomas Bermudez <bermudeztomas06@gmail.com>
2025-11-01 23:32:35 -0500

    agregada la barra de navegación básica y empezando con la vista de cursos

--- Proyecto/SAFE/courses/views.py
+++ Proyecto/SAFE/courses/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render, redirect
 from django.views.decorators.http import require_POST
 from . import models
+
+from shared.db import Database
+
+db = Database()
+
+def catalog(request):
+    usuarios = (
+        db.manager(models.Course)
+        .order_by("id")
+        .values("id", "name", "description")
+    )
+    return render(request, "courses/catalog.html", {"usuarios": list(usuarios)})
