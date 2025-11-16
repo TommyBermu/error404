62b273817354636c301ab7073a8efe19aa18d04a
David Herrera <daherreran@unal.edu.co>
2025-11-16 10:55:14 -0500

    Tests for uniqueUsername, Password and uploadExam were created, also the UI for create_exam

--- Proyecto/SAFE/courses/urls.py
+++ Proyecto/SAFE/courses/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("catalog/", views.catalog, name="catalog"),
+    path('create-exam/', views.create_exam_view, name='create_exam'),
 ]
--- Proyecto/SAFE/courses/views.py
+++ Proyecto/SAFE/courses/views.py
@@ -3,5 +3,45 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from .models import Course, Module, Content
+from .forms import QuestionUploadForm
+from django.contrib import messages
+
+
 def catalog(request):
     return render(request, "courses/catalog.html")
+
+
+# Importar la futura función que analizará el archivo
+
+def create_exam_view(request):
+    
+    if request.method == 'POST':
+        form = QuestionUploadForm(request.POST, request.FILES)
+
+        if form.is_valid():
+           
+            course = form.cleaned_data['course']
+            difficulty = form.cleaned_data['difficulty']
+            uploaded_file = form.cleaned_data['file']
+            
+            # --- 4. Lógica de Negocio (Pendiente) ---
+            # Aquí llamaremos a 'analyze_questions_file'
+            
+            messages.success(request, f"¡Formulario válido! Archivo '{uploaded_file.name}' listo para procesar.")
+
+           
+            return redirect('create_exam') 
+        
+        else:
+            messages.error(request, "El formulario tiene errores. Por favor, revísalo.")
+    
+    else:
+        
+        form = QuestionUploadForm()
+
+   
+    return render(request, 'courses/create_exam.html', {
+        'form': form
+    })
+    
+    
